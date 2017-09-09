8b4cb2cc08305b97c2f87a7cb838fcc364691326
Calpa Liu <calpaliu@gmail.com>
2017-09-09 21:20:34 +0800

    Install deck.gl

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>deck.gl 教學</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
